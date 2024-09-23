5927de623890d0ef15cf27a380d176644fae9c84
VietDoan <vietdoan177@gmail.com>
2024-09-23 21:33:24 +0700

    Refactor code to adhere to consistent coding style

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const webPush = require('web-push');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const express = require("express");
+const webPush = require("web-push");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
@@ -17,35 +17,47 @@ const publicVapidKey = process.env.VAPID_PUBLIC_KEY;
 const privateVapidKey = process.env.VAPID_PRIVATE_KEY;
 
 webPush.setVapidDetails(
-  'mailto:your-email@example.com',
+  "mailto:your-email@example.com",
   publicVapidKey,
   privateVapidKey
 );
 
-// Store subscriptions (in-memory for this example)
-const subscriptions = new Set();
+const dataStore = {};
 
 // Routes
-app.post('/subscribe', (req, res) => {
-  const subscription = req.body;
-  subscriptions.add(subscription);
-  console.log(subscriptions);
-  res.status(201).json({});
+app.post("/subscribe", (req, res) => {
+  const { fingerprint, addressWallet, subscription } = req.body;
+
+  const message = dataStore[fingerprint]
+    ? "Fingerprint đã tồn tại. Đã cập nhật addressWallet và subscription."
+    : "Đã thêm mới fingerprint.";
+    
+  // Đặt hoặc cập nhật luôn thông tin cho fingerprint
+  dataStore[fingerprint] = { addressWallet, subscription };
+
+
+  res.status(201).json({ message, dataStore });
 });
 
-app.post('/send-notification', (req, res) => {
-  const payload = JSON.stringify({ title: 'Push notification', body: 'This is a push notification!' });
-  
-  subscriptions.forEach(async subscription => {
-    console.log(subscription)
-    await webPush.sendNotification(subscription, payload).then(()=>{
-      console.log('Notification sent');
-    }).catch(error => {
-      console.error(error);
-      subscriptions.delete(subscription);
-    });
+app.post("/send-notification", (req, res) => {
+  const payload = JSON.stringify({
+    title: "Push notification",
+    body: "This is a push notification!",
   });
-  
+
+  subscriptions.forEach(async (subscription) => {
+    console.log(subscription);
+    await webPush
+      .sendNotification(subscription, payload)
+      .then(() => {
+        console.log("Notification sent");
+      })
+      .catch((error) => {
+        console.error(error);
+        subscriptions.delete(subscription);
+      });
+  });
+
   res.status(200).json({});
 });
 
